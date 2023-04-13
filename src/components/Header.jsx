cef06e40f1d03bd651845982e91900f7c7d2526a
Gilliady <gilliadyazevedo@gmail.com>
2023-04-13 16:23:26 -0300

    finalizado requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+import '../style/header.css';
 
 export class Header extends Component {
   state = {
@@ -13,14 +15,22 @@ export class Header extends Component {
 
   awaitGetUser = async () => {
     const user = await getUser();
-    const content = <h1 data-testid="header-user-name">{user.name}</h1>;
+    const content = (
+      <>
+        <h3 data-testid="header-user-name">{user.name}</h3>
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Pesquisar</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favoritos</Link>
+          <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+        </nav>
+      </>);
     this.setState({ content });
   };
 
   render() {
     const { content } = this.state;
     return (
-      <header data-testid="header-component">{ content }</header>
+      <header className="header" data-testid="header-component">{ content }</header>
     );
   }
 }
